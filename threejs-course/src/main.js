2b7d3481480573501af27006366676f5e1e6240a
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-04 16:58:57 +0530

    initialize the camera

--- threejs-course/src/main.js
+++ threejs-course/src/main.js
@@ -12,4 +12,17 @@ const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 // Adding Mesh to the Scene
 scene.add(cubeMesh);
 
-console.log(scene);
+// initialize the camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight, // aspect ratio
+  0.1, // anything closer than this can't be seen
+  30 // anything farther than this can't be seen
+);
+
+// position the camera
+camera.position.z = 5;
+
+// scene.add(camera); // even camera can be added to the scene
+
+// console.log(window.innerWidth, window.innerHeight);
