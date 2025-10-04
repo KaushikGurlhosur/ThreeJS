232ca75c5ca2b414aceb290c98618c460effa2b6
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-04 16:50:39 +0530

    creating a scene, mesh - adding the created mesh to the scene

--- threejs-course/src/main.js
+++ threejs-course/src/main.js
@@ -1,3 +1,15 @@
 import * as THREE from "three";
 
-console.log(THREE);
+// Creating a scene
+const scene = new THREE.Scene();
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "gray" });
+
+// Creating a Mesh - which accepts a Geometry and a Material
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// Adding Mesh to the Scene
+scene.add(cubeMesh);
+
+console.log(scene);
