8ebf7fa171930995f8b02a2c8b1e6170a75a2c46
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-05 13:24:51 +0530

    instantiate the controls - OrbitControls - and renderlopp to capture 60fps/120fps or more snapshot per second and adding Orbitcontrols helps to add controls to the geometry

--- camera_and_renderloop_starter/src/script.js
+++ camera_and_renderloop_starter/src/script.js
@@ -15,7 +15,7 @@ scene.add(cubeMesh);
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  50,
+  50, //fov
   window.innerWidth / window.innerHeight, // aspect ratio
   0.1, // anything closer than this can't be seen
   30 // anything farther than this can't be seen
@@ -27,10 +27,24 @@ camera.position.z = 5;
 // initialize the renderer
 const canvas = document.querySelector("canvas.threejs");
 
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true, // smooths edges of geometry
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // instantiate the controls
 const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5; // To increase the autoRotateSpeed - by default its 2
+
+// This fun renders based on devices refresh rate - 60fps / 120fps or more
+const renderloop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
