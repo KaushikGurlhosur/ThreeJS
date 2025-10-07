60a055736a03e412eb3f80c6ee7140f8cc69dc8d
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-07 13:43:31 +0530

    Rotation and Rotation Order

--- camera_and_renderloop_Transforming position/src/rotation-rotation Order.js
+++ camera_and_renderloop_Transforming position/src/rotation-rotation Order.js
@@ -11,45 +11,33 @@ const cubeMaterial = new THREE.MeshStandardMaterial({
   color: "red",
   metalness: 0.4, // how metallic it looks (0 = matte, 1 = mirror-like)
   roughness: 0.1, // lower = shinier surface
+  wireframe: true,
 });
 
 // Creating a Mesh - which accepts a Geometry and a Material
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-// Creating a group mesh
-// _____________________________
-const cubeMesh2 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cubeMesh2.position.x = -2;
-const cubeMesh3 = new THREE.Mesh(cubeGeometry, cubeMaterial);
-cubeMesh3.position.x = 2;
+// Adding Mesh to the Scene
+scene.add(cubeMesh);
 
-const group = new THREE.Group();
-group.add(cubeMesh);
-group.add(cubeMesh2);
-group.add(cubeMesh3);
+cubeMesh.rotation.reorder("YXZ"); // The order of rotation.
 
-// group.scale.y = 2; // Applies to all the cubemesh in the group
-
-scene.add(group);
-// _____________________________
-
-// // Adding Mesh to the Scene
-// scene.add(cubeMesh);
+// Defining rotation
+cubeMesh.rotation.y = THREE.MathUtils.degToRad(90);
+cubeMesh.rotation.x = THREE.MathUtils.degToRad(40);
 
 // _____________________________
 // Transforming scale
-cubeMesh.scale.set(2, 2, 1); // X, Y, Z
+// cubeMesh.scale.set(1, 1, 1); // X, Y, Z
 // _____________________________
 
-// _____________________________
-// // Trasforming position
-// cubeMesh.position.x = 1;
-// cubeMesh.position.z = -1;
-// cubeMesh.position.y = 1;
+// // 3D Axis
+// const axesHelper = new THREE.AxesHelper(2);
+// scene.add(axesHelper);
 
-// 3D Axis
+// Adding it to the cubemesh
 const axesHelper = new THREE.AxesHelper(2);
-scene.add(axesHelper);
+cubeMesh.add(axesHelper);
 
 // _____________________________
 
@@ -60,18 +48,6 @@ const camera = new THREE.PerspectiveCamera(
   0.1, // anything closer than this can't be seen
   30 // anything farther than this can't be seen
 );
-
-// const aspectRatio = window.innerWidth / window.innerHeight;
-
-// // initialize the OrthographicCamera camera
-// const camera = new THREE.OrthographicCamera(
-//   -1 * aspectRatio,
-//   1 * aspectRatio,
-//   1,
-//   -1,
-//   0.1,
-//   200
-// );
 
 // position the camera
 camera.position.z = 5;
@@ -103,13 +79,13 @@ console.log(window.devicePixelRatio);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-controls.autoRotate = true;
+// controls.autoRotate = true;
 controls.autoRotateSpeed = 5; // To increase the autoRotateSpeed - by default its 2
 
 // Resizing
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  camera.updateProjectionMatrix(); // 🔑 recalculate projection
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
