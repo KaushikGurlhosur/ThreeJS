8a81a562636e8c06dfee28cef7ec40c3a52e2f3c
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-10-27 16:39:26 +0530

    Added mesh and added group to the scene - also removed large texture tracking

--- Initial setup/src/mesh-textures.js
+++ Initial setup/src/mesh-textures.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 import { Pane } from "tweakpane";
 
 // initialize the scene
@@ -40,22 +41,22 @@ const grassRoughness = textureLoader.load(
 
 // Load the Ground Wood Chips textures
 const groundWoodAlbedo = textureLoader.load(
-  "./textures/GroundWoodChips001/GroundWoodChips001_COL_8K.jpg"
+  "./textures/GroundWoodChips001/GroundWoodChips001_COL_2K.jpg"
 );
 const groundWoodAo = textureLoader.load(
-  "./textures/GroundWoodChips001/GroundWoodChips001_AO_8K.jpg"
+  "./textures/GroundWoodChips001/GroundWoodChips001_AO_2K.jpg"
 );
 const groundWoodHeight = textureLoader.load(
-  "./textures/GroundWoodChips001/GroundWoodChips001_DISP_8K.jpg"
+  "./textures/GroundWoodChips001/GroundWoodChips001_DISP_2K.jpg"
 );
 const groundWoodNormal = textureLoader.load(
-  "./textures/GroundWoodChips001/GroundWoodChips001_NRM_8K.jpg"
+  "./textures/GroundWoodChips001/GroundWoodChips001_NRM_2K.jpg"
 );
 const groundWoodRoughness = textureLoader.load(
-  "./textures/GroundWoodChips001/GroundWoodChips001_GLOSS_8K.jpg"
+  "./textures/GroundWoodChips001/GroundWoodChips001_GLOSS_2K.jpg"
 );
 const groundWoodMetallic = textureLoader.load(
-  "./textures/GroundWoodChips001/GroundWoodChips001_REFL_8K.jpg"
+  "./textures/GroundWoodChips001/GroundWoodChips001_REFL_2K.jpg"
 );
 
 // load rocky textures
@@ -198,3 +199,55 @@ group.add(grass, groundwood, rocky);
 
 // adding the group to the scene
 scene.add(group);
+
+// initialize the light
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+
+const pointLight = new THREE.PointLight(0xffffff, 200);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+// initialize the camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10000
+);
+camera.position.z = 10;
+camera.position.y = 5;
+
+// initialize the renderer
+const canvas = document.querySelector("canvas.threejs");
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// instantiate the controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+// render the scene
+const renderloop = () => {
+  // group.children.forEach((child) =>{
+  //   if (child instanceof THREE.Mesh) {
+  //     child.rotation.y += 0.01
+  //   }
+  // })
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(renderloop);
+};
+
+renderloop();
